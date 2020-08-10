e25d7adb00e6f8b2acb1a30f6fe3e4b0af0aca70
Vascord <duarte-vasco@outlook.com>
2020-08-10 10:14:48 +0100

    First commit

---
+++ LP1-Epoca_Especial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LP1_Epoca_Especial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
