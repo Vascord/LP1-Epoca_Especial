a308533efae82c9ce5e0ad0af03f0869d8e8eabc
Vascord <duarte-vasco@outlook.com>
2020-08-25 11:44:04 +0100

    Comment Program and creat Simulator

--- LP1-Epoca_Especial/Program.cs
+++ LP1-Epoca_Especial/Program.cs
@@ -1,16 +1,26 @@
-﻿using System;
-
-namespace LP1_Epoca_Especial
+﻿namespace LP1_Epoca_Especial
 {
+    /// <summary>
+    /// Main class initiated at the beginning of the program
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Static void which is the first methods that is used. It is used
+        /// to launch the the struct to have the properties of the file in 
+        /// args, and to launch the simulator itself.
+        /// </summary>
+        /// <param name="args">The arguments of the user, used to have the file 
+        /// name with the properties.</param>
         static void Main(string[] args)
         {
             //This will see the file used by the user and see the arguments 
             // wrote in it for the simulation
             Properties properties = Properties.ReadFile(args);
-            //Creates the simulation with the arguments of the file;
-            // WIP
+            //Creates the simulation with the arguments of the file
+            Simulator simulation = new Simulator(properties);
+            //Starts the simulation
+            simulation.CoreLoop();
         }
     }
 }
---
+++ LP1-Epoca_Especial/Simulator.cs
@@ -0,0 +1,30 @@
+namespace LP1_Epoca_Especial
+{
+    public class Simulator
+    {
+        /// <summary>
+        /// Simulation properties.
+        /// </summary>
+        private Properties prop;
+        
+        /// <summary>
+        /// Class constructor.
+        /// </summary>
+        /// <param name="properties">Simulation properties</param>
+        public Simulator(Properties properties)
+        {
+            // Initializes instance variables
+
+            this.prop = properties;
+        }
+
+        /// <summary>
+        /// Main loop of the simulation. Will stop at the end of X turns,
+        /// which where choose be the user in properties.
+        /// </summary>
+        public void CoreLoop()
+        {
+            // WIP
+        }
+    }
+}
